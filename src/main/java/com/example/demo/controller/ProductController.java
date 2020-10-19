afdecee15cbb331e2b0e11d92ed0e8b7ddfd5735
Oleksandr Duliak <sdulyak@gmail.com>
2020-10-19 19:43:43 +0300

    simple spring was updated

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -1,10 +1,13 @@
 package com.example.demo.controller;
 
+import com.example.demo.CreateProductRequest;
 import com.example.demo.model.Product;
 import com.example.demo.service.ProductService;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,29 +19,37 @@ public class ProductController {
 
     ProductService productService;
 
-    @GetMapping
+    @GetMapping("/products")
     public List<Product> getAll() {
         return productService.getAll();
     }
 
-    @GetMapping(value = "/{id}")
-    public Product get(@PathVariable Long id) {
+    @GetMapping("/products/{id}")
+    public Product readProduct(@PathVariable Long id) {
         return productService.findById(id);
     }
 
-    @PostMapping
-    public void post(@RequestBody Product product) {
-        productService.add(product);
-
+    @PostMapping("/products")
+    public ResponseEntity<Product> createProduct(@RequestBody CreateProductRequest request) {
+        Product product1 = new Product();
+        product1.setPrice(request.getPrice());
+        product1.setName(request.getName());
+        productService.add(product1);
+        return ResponseEntity.ok(product1);
     }
 
-    @PutMapping("/{id}")
-    public void updateProduct(@PathVariable Long id, @RequestBody Product product) {
-        productService.add(product);
+    @PutMapping("/products")
+    public ResponseEntity<Product> updateProduct(@RequestBody CreateProductRequest request) {
+        Product product1 = productService.findById(request.getId());
+        product1.setPrice(request.getPrice());
+        product1.setName(request.getName());
+        productService.add(product1);
+        return ResponseEntity.ok(product1);
     }
 
-    @DeleteMapping("/{id}")
-    public void removeProduct(@PathVariable Long id) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/products/{id}")
+    public void deleteProduct(@PathVariable Long id) {
         Product product = productService.findById(id);
         productService.remove(product);
     }
